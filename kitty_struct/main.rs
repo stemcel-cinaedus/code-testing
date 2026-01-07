639c04486c2f8d7c353d8db635d52db7174d354a
stemcel-cinaedus <frogscooper@gmail.com>
2026-01-07 00:40:31 +0000

    Add files via upload

--- kitty_struct/main.rs
+++ kitty_struct/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-struct kitty {
+struct Kitty {
     alive: bool,
     fur_color: String,
     name: String,
@@ -8,27 +8,42 @@ struct kitty {
 }
 
 fn main() {
-    let mut kitty1 = kitty {
+    let mut kitty1 = Kitty {
         alive: true,
         fur_color: String::from("Calico"),
         name: String::from("Mary Magdalene"),
         leg_count: 4,
     };
 
-    let mut new_cat_name = String::new();
-    let mut new_cat_fur = String::new();
-    println!("What would you like to name your new cat?");
-    let mut new_cat_name = io::stdin().read_line(&mut new_cat_name).expect("Failed to read name.");
-    let new_cat_name = new_cat_name.to_string();
-    println!("What color is its fur?");
-    let new_cat_fur = io::stdin().read_line(&mut new_cat_fur).expect("Failed to read fur color.");
-    let new_cat_fur = new_cat_fur.to_string();
-    birth_kitten(new_cat_fur, new_cat_name);
+    let mut collection = vec![];
+    loop {
+        let mut new_cat_name = String::new();
+        let mut new_cat_fur = String::new();
+        let mut answer = String::new();
+        
+        println!("What would you like to name your new cat?");
+        let mut new_cat_name = io::stdin().read_line(&mut new_cat_name).expect("Failed to read name.");
+        let new_cat_name = new_cat_name.to_string();
+        println!("What color is its fur?");
+        let new_cat_fur = io::stdin().read_line(&mut new_cat_fur).expect("Failed to read fur color.");
+        let new_cat_fur = new_cat_fur.to_string();
+        collection.push(birth_kitten(new_cat_fur, new_cat_name));
+        
+        println!("Would you like to keep adding cats? Answer with Y or N");
+        let mut answer = io::stdin().read_line(&mut answer).expect("Couldn't read input");
+        let mut answer = answer.to_string();
+        let answer = answer.trim().to_uppercase();
+        match &answer[..] {
+            "Y" => continue,
+            "N" => break,
+            _ => println!("Invalid input"),
+        }
+    }
 
 }
 
-fn birth_kitten(fur_color: String, name: String) -> kitty {
-    kitty {
+fn birth_kitten(fur_color: String, name: String) -> Kitty {
+    Kitty {
     alive: true, //There are no stillbirths in this program
     fur_color,
     name,
