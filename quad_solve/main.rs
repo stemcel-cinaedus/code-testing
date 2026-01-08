f6a9168079805f365f8e859f4470c20f1d473aa1
stemcel-cinaedus <frogscooper@gmail.com>
2026-01-08 20:40:56 +0000

    Add files via upload
    I have not compiled this yet so there are probably a ton of errors. It is also not finished, I have not added the actual solver function yet (should be easier than parsing the string and error handling however).

---
+++ quad_solve/main.rs
@@ -0,0 +1,19 @@
+struct polynomial(i32, i32, i32);
+
+fn main() {
+    let mut input = String::new();
+    let mut a: i32 = _;
+    let mut b: i32 = _;
+    let mut c: i32 =_;
+    println!("Input a, b, and c of the quadratic polynomial, seperated by spaces.");
+    io::stdin().read_line(&mut input_s).expect("Couldn't read input.");
+    let mut str_vals: Vec<&str> = text.split(" ").collect();
+    let quadratic_vals = vec!()
+    for string_value in str_vals {
+        let num = match num.trim().parse() {
+            Ok(num) => num,
+            Err(_) => panic,
+        };
+        quadratic_vals.append(num);
+    }
+}
