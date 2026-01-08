315c1c916a33f36f93396a2b6edf17061f35f210
stemcel-cinaedus <frogscooper@gmail.com>
2026-01-08 21:34:42 +0000

    Add files via upload
    I managed to get a version that compiles without errors. I'll have to test it later, as I have to leave the library because it is about to close. It's such a major change that a commit is necessary anyways.

--- quad_solve/main.rs
+++ quad_solve/main.rs
@@ -1,19 +1,33 @@
-struct polynomial(i32, i32, i32);
+use std::io;
+//use core::f64::math::sqrt;
+//use core::panicking;
+//struct polynomial(i32, i32, i32);
 
 fn main() {
     let mut input = String::new();
-    let mut a: i32 = _;
-    let mut b: i32 = _;
-    let mut c: i32 =_;
+    //let mut a: i32 = 0;
+    //let mut b: i32 = 0;
+    //let mut c: i32 = 0;
     println!("Input a, b, and c of the quadratic polynomial, seperated by spaces.");
-    io::stdin().read_line(&mut input_s).expect("Couldn't read input.");
-    let mut str_vals: Vec<&str> = text.split(" ").collect();
-    let quadratic_vals = vec!()
+    io::stdin().read_line(&mut input).expect("Couldn't read input.");
+    let mut str_vals: Vec<&str> = input.split(" ").collect();
+    let mut quadratic_vals = vec!();
     for string_value in str_vals {
-        let num = match num.trim().parse() {
+        let num: f64 = match string_value.trim().parse() {
             Ok(num) => num,
-            Err(_) => panic,
+            Err(_) => continue,
         };
-        quadratic_vals.append(num);
-    }
+        quadratic_vals.push(num);
+     }
+     let solved = quadratic_formula(quadratic_vals[0], quadratic_vals[1], quadratic_vals[2]);
+     println!("The roots are {} and {}", solved.0, solved.1)
+
+}
+
+fn quadratic_formula(a: f64, b: f64, c: f64) -> (f64, f64) {
+    let root1 = (-b + f64::sqrt((b * b) * f64::from(4) * a * c)) / (f64::from(2) * a);
+    let root2 = (-b - f64::sqrt((b * b) * f64::from(4) * a * c)) / (f64::from(2) * a);
+    
+    return(root1, root2)
+
 }
