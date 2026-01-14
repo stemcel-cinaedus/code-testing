7e71a9a0582728e040f4cbc7f3d7afe3fdacf8d7
stemcel-cinaedus <frogscooper@gmail.com>
2026-01-14 21:33:29 +0000

    Add files via upload
    Week 2 Java assignment (finished in advance)

---
+++ week2.java
@@ -0,0 +1,7 @@
+public class week2 {
+  public static void main(String[] args) {
+    System.out.print("This line is actually");
+    System.out.println(" 2 different statments, for no good reason.");
+    System.out.printf("%s%d%s%n%s%f%n", "Thank you giving me ", 50, " million dollars.", "I used all of them except for $", 5.16);
+  }
+}
