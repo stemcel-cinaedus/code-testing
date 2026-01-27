f3a883b4b26a88ed0b369418a115bb1f30342bf9
stemcel-cinaedus <frogscooper@gmail.com>
2026-01-27 23:52:35 +0000

    Add files via upload
    I ended up iterating over the string by using graphemes from the unicode segmentation crate.

--- math/main.rs
+++ math/main.rs
@@ -1,14 +1,15 @@
 use std::io;
+use unicode_segmentation::UnicodeSegmentation;
 
 fn main() {
     let mut input_expression = String::new();
     let mut floats_present: bool = false;
     io::stdin().read_line(&mut input_expression);
-    let mut input_expression: Vec<char> = input_expression.chars().collect();
-    let mut shunting_yard_stack: Vec<char> = Vec::new();
-    let mut shunting_yard_output: Vec<char> = Vec::new();
+    // let mut input_expression: Vec<char> = input_expression.chars().collect();
+    let mut shunting_yard_stack: Vec<&str> = Vec::new();
+    let mut shunting_yard_output: Vec<&str> = Vec::new();
     let mut float_output: Vec<f64> = Vec::new();
-    for symbol in input_expression {
+    for symbol in input_expression.graphemes(true) {
         match symbol {
             "(" | ")" | "+" | "-" | "*" | "/" | "%" | "^" => shunting_yard_stack.push(symbol),
             "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" => {
