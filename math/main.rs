b7463bc7893406ff602e567abe39d04afd689984
stemcel-cinaedus <frogscooper@gmail.com>
2026-01-29 06:41:35 +0000

    Add files via upload
    I misread the Reverse Polish Notation explanation....

--- math/main.rs
+++ math/main.rs
@@ -3,28 +3,25 @@ use unicode_segmentation::UnicodeSegmentation;
 
 fn main() {
     let mut input_expression = String::new();
-    let mut floats_present: bool = false;
     io::stdin().read_line(&mut input_expression);
     // let mut input_expression: Vec<char> = input_expression.chars().collect();
     let mut shunting_yard_stack: Vec<&str> = Vec::new();
-    let mut shunting_yard_output: Vec<&str> = Vec::new();
-    let mut float_output: Vec<f64> = Vec::new();
+    let mut input_expression_nums: Vec<&str> = Vec::new();
+    let mut expression_output: Vec<f64> = Vec::new();
     for symbol in input_expression.graphemes(true) {
         match symbol {
             "(" | ")" | "+" | "-" | "*" | "/" | "%" | "^" => shunting_yard_stack.push(symbol),
             "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" => {
-                shunting_yard_output.push(symbol)
+                input_expression_nums.push(symbol)
             }
             _ => (),
         };
     }
-    for num in shunting_yard_output {
-        let num: f64 = match num.parse() {
+    for num in input_expression_nums {
+        let mut num: f64 = match num.parse() {
             Ok(num) => num,
             Err(num) => continue,
         };
-        float_output.push(num);
+        expression_output.push(num);
     }
 }
-
-fn evaluate_expression(float_output: Vec<f64>, shunting_yard_stack: Vec<char>) {}
